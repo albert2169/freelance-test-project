d8a014548857eee0725db4b4a4b25089bf24a194
Albert Aghababyan <albertaghababyan@Alberts-Mac-mini.local>
2025-07-09 10:13:11 -0700

    refactor the code

--- lib/presentation/models/delivery_details_view_model.dart
+++ lib/presentation/models/delivery_details_view_model.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 @immutable
@@ -8,38 +7,9 @@ class DeliveryDetailsViewModel {
   final String comments;
   final String pickupAddress;
   const DeliveryDetailsViewModel({
-    required this.address, required this.comments, required this.contactNumber, required this.pickupAddress
+    required this.address,
+    required this.comments,
+    required this.contactNumber,
+    required this.pickupAddress,
   });
-
-  // DeliveryDetailsViewModel copyWith({
-  
-  // }) {
-  //   return DeliveryDetailsViewModel(
-  //   );
-  // }
-
-
-
-  // @override
-  // bool operator ==(Object other) {
-  //   if (identical(this, other)) return true;
-
-  //   return other is DeliveryDetailsViewModel &&
-  //       other.id == id &&
-  //       other.name == name &&
-  //       other.priceInfo == priceInfo &&
-  //       other.displayImage == displayImage &&
-  //       other.description == description &&
-  //       other.productCategory == productCategory;
-  // }
-
-  // @override
-  // int get hashCode {
-  //   return id.hashCode ^
-  //       name.hashCode ^
-  //       priceInfo.hashCode ^
-  //       displayImage.hashCode ^
-  //       description.hashCode ^
-  //       productCategory.hashCode;
-  // }
 }
